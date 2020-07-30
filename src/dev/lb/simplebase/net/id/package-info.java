8f013d4ab153c902b35a5d0136f9ef7e10a14784
ReBuilders101 <rebuilders101@gmail.com>
2020-07-30 14:44:29 +0200

    package-info for id package

---
+++ src/dev/lb/simplebase/net/id/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes related to {@link dev.lb.simplebase.net.id.NetworkID}s. Instances identify
+ * a participant in network traffic.
+ * <p>
+ * Instances are created with static factory methods in the {@code NetworkID} class.
+ */
+package dev.lb.simplebase.net.id;
